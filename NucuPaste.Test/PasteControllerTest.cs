ec20466cb417961f260609385237b6bb804d4067
Denis Nuțiu <denis.nutiu@gmail.com>
2019-05-07 01:48:38 +0300

    Update test to use in memory database

--- NucuPaste.Test/PasteControllerTest.cs
+++ NucuPaste.Test/PasteControllerTest.cs
@@ -10,20 +10,17 @@ using NucuPaste.Models;
 using Xunit;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using Xunit.Abstractions;
 
 namespace NucuPaste.Test
 {
     public class PasteControllerTest
     {
-        internal static Mock<DbSet<T>> GetMockDbSet<T>(ICollection<T> entities) where T : class
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public PasteControllerTest(ITestOutputHelper testOutputHelper)
         {
-            var mockSet = new Mock<DbSet<T>>();
-            mockSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(entities.AsQueryable().Provider);
-            mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(entities.AsQueryable().Expression);
-            mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(entities.AsQueryable().ElementType);
-            mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(entities.AsQueryable().GetEnumerator());
-            mockSet.Setup(m => m.Add(It.IsAny<T>())).Callback<T>(entities.Add);
-            return mockSet;
+            _testOutputHelper = testOutputHelper;
         }
 
         [Fact]
@@ -31,25 +28,34 @@ namespace NucuPaste.Test
         public void PasteController_GetAction()
         {
             // Arrange
-            var mockPasteDbContext = new Mock<PasteDbContext>();
+            var options = new DbContextOptionsBuilder<PasteDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDB")
+                .Options;
             var mockPasteLogger = new Mock<ILogger<PastesController>>();
             
             var fixture = new Fixture();
-            IList<Paste> pastes = new List<Paste>
+            var pastes = new List<Paste>
             {
                 fixture.Build<Paste>().Create(),
                 fixture.Build<Paste>().Create(),
                 fixture.Build<Paste>().Create()
             };
-            
-            var mockDbSet = GetMockDbSet<Paste>(pastes);
-            mockPasteDbContext.Setup(c => c.Pastes).Returns(mockDbSet.Object);
 
-            var pasteController = new PastesController(mockPasteDbContext.Object, mockPasteLogger.Object);
             
             // Act
-            List<Paste> results = pasteController.GetPastes().ToList();
+            List<Paste> results;
+            using (var context = new PasteDbContext(options))
+            {
+                foreach (var p in pastes)
+                {
+                    context.Pastes.Add(p);
+                }
 
+                context.SaveChanges();
+                
+                var pasteController = new PastesController(context, mockPasteLogger.Object);
+                results = pasteController.GetPastes().ToList();
+            }
 
             // Assert
             for (var i = 0; i < pastes.Count; i++) {
