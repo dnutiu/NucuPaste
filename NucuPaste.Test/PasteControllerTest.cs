6341f12f76ff69ca49e7bde2c7cf1cc4d08a73ec
Nutiu Denis-Cosmin <denis.nutiu@gmail.com>
2019-05-06 22:44:15 +0300

    Add sample test project

---
+++ NucuPaste.Test/PasteControllerTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace NucuPaste.Test
+{
+    public class PasteControllerTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true, "Demo.");
+        }
+    }
+}
